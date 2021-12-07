07f953b59cecf537cf66e7b230ede2ded760ea9c
fwf <Pytato@users.noreply.github.com>
2021-12-07 23:49:59 +0000

    Add `event_loop` initialisation kwarg to Downloader
    In the case of optional kwarg being unfilled, defaults to asyncio.get_event_loop()

--- bot/utils/downloader.py
+++ bot/utils/downloader.py
@@ -3,6 +3,7 @@ import youtube_dl
 from concurrent.futures import ThreadPoolExecutor
 from functools import partial
 
+import asyncio
 
 ytdl_options = {
     "format": "bestaudio/best",
@@ -11,15 +12,16 @@ ytdl_options = {
 
 
 class Downloader:
-    def __init__(self, output_dir=None, max_workers=4) -> None:
+    def __init__(self, event_loop=None, output_dir=None, max_workers=4) -> None:
         self.__pool = ThreadPoolExecutor(max_workers=max_workers)
         self.ytdl = youtube_dl.YoutubeDL(ytdl_options)
+        self.loop = event_loop if event_loop is not None else asyncio.get_event_loop()
         if output_dir is not None:
             pass
 
-    async def get(self, loop, *args, **kwargs):
+    async def get(self, *args, **kwargs):
         """Runs an instance of YoutubeDL extract_info in the threadpool, returning a future
         that'll shove itself into the event loop when it's done.
         This will require work for the ungodly errors that can come out of YTDL.
         """
-        return await loop.run_in_executor(self.__pool, partial(self.ytdl.extract_info, *args, **kwargs))
+        return await self.loop.run_in_executor(self.__pool, partial(self.ytdl.extract_info, *args, **kwargs))
